59758e761f4200d98eaaf462da547b7a56c9b680
PrsRicky15 <prashantsingh1993@gmail.com>
2025-09-21 09:24:17 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func basic() {
+	var MyName string
+	fmt.Println("Enter your name\n")
+	_, _ = fmt.Scan(&MyName)
+	fmt.Println("My name is ", MyName, " and I am an Engineer")
+
+	var age int
+	var months int
+	age = 20
+	months = 4
+	fmt.Printf("My age is %v years and %v months\n", age, months)
+}
+
+func main() {
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	for i := 1; i <= 5; i++ {
+		fmt.Println("i =", 100/i)
+	}
+
+	basic()
+}
