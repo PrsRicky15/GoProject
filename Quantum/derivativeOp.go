2b3804f8927776edb5e88f2cf6be65aa38ca3185
PrsRicky15 <prashantsingh1993@gmail.com>
2025-10-01 23:31:14 +0300

    one-dim DVR Hamiltonian gen

--- Quantum/derivativeOp.go
+++ Quantum/derivativeOp.go
@@ -5,6 +5,8 @@ import (
 	"math"
 
 	"golang.org/x/exp/constraints"
+	"gonum.org/v1/gonum/blas"
+	"gonum.org/v1/gonum/blas/cblas128"
 
 	"gonum.org/v1/gonum/mat"
 )
@@ -15,7 +17,7 @@ type Number interface {
 
 type MatrixOp interface {
 	Mat()
-	GetMat() mat.Matrix
+	GetMat() *mat.Dense
 	Diagonalize() ([]float64, *mat.Dense, error)
 }
 
@@ -26,9 +28,13 @@ type CanTimeSolverOp interface {
 }
 
 type TimeSolverOp interface {
-	ExpDt(In []float64) []float64
-	ExpDtTo(In []float64, Out []float64)
-	ExpDtInPlace(InOut []float64)
+	ExpDt(Dt float64, In []float64) []float64
+	ExpDtTo(Dt float64, In []float64, Out []float64)
+	ExpDtInPlace(Dt float64, InOut []float64)
+
+	ExpIdt(Dt float64, In []complex64) []complex64
+	ExpIdtTo(Dt float64, In []complex64, Out []complex64)
+	ExpIdtInPlace(Dt float64, InOut []complex64)
 }
 
 type MomentumOp interface {
@@ -62,15 +68,19 @@ type MomDvrBasis struct {
 }
 
 type CanonicalKeDvrBasis struct {
-	grid *gridData.RadGrid
-	mass float64
-	kMat *mat.Dense
+	grid  *gridData.RadGrid
+	mass  float64
+	kMat  *mat.Dense
+	uMat  *mat.Dense
+	keVal []float64
 }
 
 type KeDvrBasis struct {
-	grid *gridData.RadGrid
-	mass float64
-	kMat *mat.Dense
+	grid  *gridData.RadGrid
+	mass  float64
+	kMat  *mat.Dense
+	uMat  *mat.Dense
+	keVal []float64
 }
 
 func NewKeDVR(grid *gridData.RadGrid, mass float64) *KeDvrBasis {
@@ -80,17 +90,17 @@ func NewKeDVR(grid *gridData.RadGrid, mass float64) *KeDvrBasis {
 	}
 }
 
-func (k *KeDvrBasis) Mat() {
-	dim := int(k.grid.NPoints())
+func (k *KeDvrBasis) mInfinityToInfinity(keMat *mat.Dense) {
+	_, dim := keMat.Dims()
 	dx2 := k.grid.DeltaR() * k.grid.DeltaR()
 	massDx2 := k.mass * dx2
 	diagTerm := math.Pi * math.Pi / (6. * massDx2)
 	invMassDx2 := 1.0 / massDx2
 
-	k.kMat = mat.NewDense(dim, dim, nil)
+	keMat = mat.NewDense(dim, dim, nil)
 
 	for i := 0; i < dim; i++ {
-		k.kMat.Set(i, i, diagTerm)
+		keMat.Set(i, i, diagTerm)
 	}
 
 	for i := 1; i < dim; i++ {
@@ -99,9 +109,42 @@ func (k *KeDvrBasis) Mat() {
 			sign := float64(1 - 2*(diff&1))
 			diffSq := float64(diff * diff)
 			kEval := sign * invMassDx2 / diffSq
-			k.kMat.Set(i, j, kEval)
-			k.kMat.Set(j, i, kEval)
+			keMat.Set(i, j, kEval)
+			keMat.Set(j, i, kEval)
 		}
+	}
+}
+
+func (k *KeDvrBasis) zeroToInfinity(keMat *mat.Dense) {
+	_, dim := keMat.Dims()
+	dx2 := k.grid.DeltaR() * k.grid.DeltaR()
+	massDx2 := k.mass * dx2
+	diagTerm := math.Pi * math.Pi / (6. * massDx2)
+	invMassDx2 := 1.0 / massDx2
+
+	for i := 0; i < dim; i++ {
+		keMat.Set(i, i, diagTerm-0.25/float64(i*i))
+	}
+
+	for i := 1; i < dim; i++ {
+		for j := 0; j < i; j++ {
+			diff := i - j
+			add := i + j
+			sign := float64(1 - 2*(diff&1))
+			diffSq := float64(diff * diff)
+			addSq := float64(add * add)
+			kEval := sign * invMassDx2 * (1./diffSq - 1./addSq)
+			keMat.Set(i, j, kEval)
+			keMat.Set(j, i, kEval)
+		}
+	}
+}
+
+func (k *KeDvrBasis) Mat() {
+	if math.Abs(k.grid.RMax()-k.grid.RMin()) <= 1 {
+		k.zeroToInfinity(k.kMat)
+	} else {
+		k.mInfinityToInfinity(k.kMat)
 	}
 }
 
@@ -111,16 +154,64 @@ func (k *KeDvrBasis) Diagonalize() ([]float64, *mat.Dense, error) {
 }
 
 func (k *KeDvrBasis) ExpDt(In []float64) []float64 {
-	//TODO implement me
-	panic("implement me")
+	expMat := mat.NewDense(int(k.grid.NPoints()), int(k.grid.NPoints()), nil)
+	expMat.Exp(k.kMat)
+	inVec := mat.NewVecDense(len(In), In)
+	outVec := mat.NewVecDense(len(In), nil)
+	outVec.MulVec(expMat, inVec)
+
+	result := make([]float64, len(In))
+	for i := 0; i < len(In); i++ {
+		result[i] = outVec.AtVec(i)
+	}
+	return result
 }
 
 func (k *KeDvrBasis) ExpDtTo(In []float64, Out []float64) {
-	//TODO implement me
-	panic("implement me")
+	expMat := mat.NewDense(int(k.grid.NPoints()), int(k.grid.NPoints()), nil)
+	expMat.Exp(k.kMat)
+	inVec := mat.NewVecDense(len(In), In)
+	outVec := mat.NewVecDense(len(In), nil)
+	outVec.MulVec(expMat, inVec)
+
+	for i := 0; i < len(In); i++ {
+		Out[i] = outVec.AtVec(i)
+	}
 }
 
 func (k *KeDvrBasis) ExpDtInPlace(InOut []float64) {
-	//TODO implement me
-	panic("implement me")
+	k.ExpDtTo(InOut, InOut)
+}
+
+func (k *KeDvrBasis) ExpIdt(In []complex128) []complex128 {
+	expMat := mat.NewCDense(int(k.grid.NPoints()), int(k.grid.NPoints()), nil)
+	result := make([]complex128, len(In))
+
+	cblas128.Gemv(
+		blas.NoTrans,
+		1,
+		expMat.RawCMatrix(),
+		cblas128.Vector{N: len(In), Data: In, Inc: 1},
+		0, // beta = 0
+		cblas128.Vector{N: len(result), Data: result, Inc: 1},
+	)
+
+	return result
+}
+
+func (k *KeDvrBasis) ExpIdtTo(In []complex128, Out []complex128) {
+	expMat := mat.NewDense(int(k.grid.NPoints()), int(k.grid.NPoints()), nil)
+
+	cblas128.Gemv(
+		blas.NoTrans,
+		1,
+		expMat.RawCMatrix(),
+		cblas128.Vector{N: len(In), Data: In, Inc: 1},
+		0, // beta = 0
+		cblas128.Vector{N: len(Out), Data: Out, Inc: 1},
+	)
+}
+
+func (k *KeDvrBasis) ExpIdtInPlace(InOut []float64) {
+	k.ExpDtTo(InOut, InOut)
 }
--- Quantum/matrixoperation.go
+++ Quantum/matrixoperation.go
@@ -3,32 +3,69 @@ package Quantum
 import (
 	"fmt"
 
+	"gonum.org/v1/gonum/blas"
+	"gonum.org/v1/gonum/blas/blas64"
+	"gonum.org/v1/gonum/lapack"
+	"gonum.org/v1/gonum/lapack/lapack64"
 	"gonum.org/v1/gonum/mat"
 )
 
-func RealDiagonalize(Op *KeDvrBasis, n int) ([]float64, *mat.Dense, error) {
-	matrix := Op.GetMat()
-
-	sym, ok := matrix.(*mat.SymDense)
+func RealDiagonalize(Op MatrixOp, evals []float64, eVecs *mat.Dense) error {
+	matrix := mat.Matrix(Op.GetMat())
+	symMat, ok := matrix.(*mat.SymDense)
 	if !ok {
-		sym = mat.NewSymDense(n, nil)
-		for i := 0; i < n; i++ {
-			for j := i; j < n; j++ {
-				sym.SetSym(i, j, matrix.At(i, j))
-			}
-		}
+		return fmt.Errorf("matrix must be *mat.SymDense")
 	}
 
 	var eig mat.EigenSym
-	if ok := eig.Factorize(sym, true); !ok {
-		return nil, nil, fmt.Errorf("failed to compute eigenvalues and eigenvectors")
+	if ok := eig.Factorize(symMat, true); !ok {
+		return fmt.Errorf("failed to compute eigenvalues and eigenvectors")
 	}
 
-	eigenvalues := make([]float64, n)
-	eig.Values(eigenvalues)
+	eig.Values(evals)
+	eig.VectorsTo(eVecs)
+	return nil
+}
 
-	eigenvectors := mat.NewDense(n, n, nil)
-	eig.VectorsTo(eigenvectors)
+func RealDiagonalizeLapack(Op MatrixOp, evals []float64, eVecs *mat.Dense) error {
+	matrix := Op.GetMat()
+	_, n := matrix.Dims()
 
-	return eigenvalues, eigenvectors, nil
+	data := matrix.RawMatrix().Data
+
+	a := blas64.Symmetric{
+		N:      n,
+		Uplo:   blas.Upper,
+		Data:   data,
+		Stride: n,
+	}
+
+	w := make([]float64, n)
+	work := make([]float64, 1)
+	lwork := -1
+
+	ok := lapack64.Syev(lapack.EVCompute, a, w, work, lwork)
+	if !ok {
+		return fmt.Errorf("LAPACK Syev query failed")
+	}
+
+	optimalLwork := int(work[0])
+	work = make([]float64, optimalLwork)
+	lwork = optimalLwork
+
+	ok = lapack64.Syev(lapack.EVCompute, a, w, work, lwork)
+	if !ok {
+		return fmt.Errorf("LAPACK Syev computation failed")
+	}
+
+	// Copy results
+	copy(evals, w)
+	eVecs.SetRawMatrix(blas64.General{
+		Rows:   n,
+		Cols:   n,
+		Stride: n,
+		Data:   a.Data,
+	})
+
+	return nil
 }
