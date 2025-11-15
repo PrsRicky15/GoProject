7091245ea1b66cf9e1d5b886edbdeb33128f2ff8
saurabbh14 <saurabh.mhatre1995@gmail.com>
2025-11-15 16:36:20 +0100

    GoAPI

--- main.go
+++ main.go
@@ -2,34 +2,33 @@ package main
 
 import (
 	"fmt"
-   // "log"
-   // "net/http"
+    "log"
+   	"net/http"
 
-    //"GoProject/Plots"
+    "GoProject/Plots"
 	"GoProject/gridData"
-   // "github.com/gorilla/mux"
-   // "github.com/rs/cors"
+    "github.com/gorilla/mux"
+    "github.com/rs/cors"
 )
 
 func main() {
-	grid, _ := NewRGrid(0, 25, 100)
-    fmt.Println("Grid created:", grid)
+	grid, _ := gridData.NewRGrid(0, 25, 100)
+	fmt.Println(grid)
 
-//	router := mux.NewRouter()
+	router := mux.NewRouter()
 
     // API routes
-//    router.HandleFunc("/api/plots/data", generatePlotData).Methods("POST")
+    router.HandleFunc("/api/plots/data", Plots.GeneratePlotData).Methods("POST")
 
     // Enable CORS
-//    c := cors.New(cors.Options{
-//        AllowedOrigins:   []string{"http://localhost:5173"},
-//        AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
-//        AllowedHeaders:   []string{"Content-Type"},
-//        AllowCredentials: true,
-//    })
+    c := cors.New(cors.Options{
+        AllowedOrigins:   []string{"http://localhost:8080"},
+        AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+        AllowedHeaders:   []string{"Content-Type"},
+        AllowCredentials: true,
+    })
+    handler := c.Handler(router)
 
-//    handler := c.Handler(router)
-
-//    log.Println("Server starting on :8080")
-//    log.Fatal(http.ListenAndServe(":8080", handler))
+    log.Println("Server starting on :8080")
+    log.Fatal(http.ListenAndServe(":8080", handler))
 }
