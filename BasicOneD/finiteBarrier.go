554e54129dbbba737bd4bd8105cca82f537bada4
PrsRicky15 <prashantsingh1993@gmail.com>
2025-11-29 10:47:28 +0200

    test composite function and numerical integration

--- BasicOneD/finiteBarrier.go
+++ BasicOneD/finiteBarrier.go
@@ -2,77 +2,35 @@ package BasicOneD
 
 import (
 	"GoProject/gridData"
-	"fmt"
 )
 
-func Barrier() {
-	grid, err := gridData.NewRGrid(-3, 8, 100)
-	if err != nil {
-		panic(err)
+type FiniteBarrier struct {
+	grid  *gridData.RadGrid
+	rFunc *gridData.Rfunc
+}
+
+func NewFiniteBarrier(grid *gridData.RadGrid, rFunc *gridData.Rfunc) *FiniteBarrier {
+
+	if grid == nil || rFunc == nil {
+		panic("grid and rFunc must not be nil")
 	}
 
-	v0x := make([]float64, 3)
-	v0x[0] = 0
-	v0x[1] = 1
-	v0x[2] = -0.5
-
-	aPoint := make([]float64, 3)
-	aPoint[0] = 0
-	aPoint[1] = 5
-	aPoint[2] = 8
-
-	myfunc := gridData.NewRectBarrier(v0x, aPoint)
-
-	fgrid := make([]float64, grid.NPoints())
-	grid.FunctionOnGridInPlace(myfunc, fgrid)
-
-	err = grid.PrintVectorToFileRe(fgrid, "barrier.dat", "%21.14e")
-	if err != nil {
-		return
+	return &FiniteBarrier{
+		grid:  grid,
+		rFunc: rFunc,
 	}
 }
 
-func GaussianBarrier() {
-	grid, err := gridData.NewRGrid(-20, 20, 200)
-	if err != nil {
-		panic(err)
-	}
-	gauss := gridData.Gaussian[float64]{Strength: 1., Sigma: 2}
-	err = grid.PrintPotentToFileRe(gauss, "GaussBarrier.dat", "%21.14e")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("gauss:", gauss)
+func (fb *FiniteBarrier) ReDefine(grid *gridData.RadGrid, rFunc *gridData.Rfunc) {
+	fb.rFunc = rFunc
+	fb.grid = grid
 }
 
-func SuperGaussianBarrier() {
-	grid, err := gridData.NewRGrid(-20, 20, 200)
-	if err != nil {
-		panic(err)
-	}
-	gauss := gridData.SuperGaussian[float64]{Strength: 1., Sigma: 2, Order: 6}
-	err = grid.PrintPotentToFileRe(gauss, "SuperGaussBarrier.dat", "%21.14e")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("gauss:", gauss)
+func (fb *FiniteBarrier) ReDefineFunc(rFunc *gridData.Rfunc) {
+	fb.rFunc = rFunc
 }
 
-func CompositeFunction() {
-	grid, err := gridData.NewRGrid(-10, 10, 200)
-	if err != nil {
-		panic(err)
-	}
-
-	funcSin := gridData.NewSin(1., 3, 0.)
-	funcGauss := gridData.Gaussian[float64]{Sigma: 2, Strength: 1.}
-	funcMult := gridData.NewProductFunc(funcSin, funcGauss)
-
-	fgrid := make([]float64, grid.NPoints())
-	grid.FunctionOnGridInPlace(funcMult, fgrid)
-
-	err = grid.PrintVectorToFileRe(fgrid, "barrier.dat", "%21.14e")
-	if err != nil {
-		return
-	}
+type TDFiniteBarrier struct {
+	barrier *FiniteBarrier
+	tdGrid  *gridData.TimeGrid
 }
---
+++ BasicOneD/systems.go
@@ -0,0 +1,78 @@
+package BasicOneD
+
+import (
+	"GoProject/gridData"
+	"fmt"
+)
+
+func BarrierPotential() {
+	grid, err := gridData.NewRGrid(-3, 8, 100)
+	if err != nil {
+		panic(err)
+	}
+
+	v0x := make([]float64, 3)
+	v0x[0] = 0
+	v0x[1] = 1
+	v0x[2] = -0.5
+
+	aPoint := make([]float64, 3)
+	aPoint[0] = 0
+	aPoint[1] = 5
+	aPoint[2] = 8
+
+	myfunc := gridData.NewRectBarrier(v0x, aPoint)
+
+	fgrid := make([]float64, grid.NPoints())
+	grid.FunctionOnGridInPlace(myfunc, fgrid)
+
+	err = grid.PrintVectorToFileRe(fgrid, "barrier.dat", "%21.14e")
+	if err != nil {
+		return
+	}
+}
+
+func GaussianBarrier() {
+	grid, err := gridData.NewRGrid(-20, 20, 200)
+	if err != nil {
+		panic(err)
+	}
+	gauss := gridData.Gaussian[float64]{Strength: 1., Sigma: 2}
+	err = grid.PrintPotentToFileRe(gauss, "GaussBarrier.dat", "%21.14e")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("gauss:", gauss)
+}
+
+func SuperGaussianBarrier() {
+	grid, err := gridData.NewRGrid(-20, 20, 200)
+	if err != nil {
+		panic(err)
+	}
+	gauss := gridData.SuperGaussian[float64]{Strength: 1., Sigma: 2, Order: 6}
+	err = grid.PrintPotentToFileRe(gauss, "SuperGaussBarrier.dat", "%21.14e")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("gauss:", gauss)
+}
+
+func CompositeFunction() {
+	grid, err := gridData.NewRGrid(-10, 10, 200)
+	if err != nil {
+		panic(err)
+	}
+
+	funcSin := gridData.NewSin(1., 3, 0.)
+	funcGauss := gridData.Gaussian[float64]{Sigma: 2, Strength: 1.}
+	funcMult := gridData.NewProductFunc(funcSin, funcGauss)
+
+	fgrid := make([]float64, grid.NPoints())
+	grid.FunctionOnGridInPlace(funcMult, fgrid)
+
+	err = grid.PrintVectorToFileRe(fgrid, "barrier.dat", "%21.14e")
+	if err != nil {
+		return
+	}
+}
